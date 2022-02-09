2a845ec49c01e058fabc1ed09da82826a66b239c
erheinrichs <97911839+erheinrichs@users.noreply.github.com>
2022-02-08 22:21:49 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-require(["esri/Map", "esri/views/MapView"], function (Map, MapView) {
-        var map = new Map({
-          basemap: "topo-vector"
+require(["esri/Map", "esri/views/MapView", "esri/layers/FeatureLayer"], (Map, MapView, FeatureLayer) => {
+        const map = new Map({
+          basemap: "dark-gray"
         });
 
-        var view = new MapView({
+        const view = new MapView({
           container: "viewDiv",
           map: map,
-          zoom: 4,
-          center: [15, 65] // longitude, latitude
+          zoom: 9,
+          center: [-71.4774, 41.5401]  
         });
-      });
+
+  const featureLayer_2 = new FeatureLayer({
+          url: "https://services2.arcgis.com/S8zZg9pg23JUEexQ/arcgis/rest/services/StateBoundaries/FeatureServer"
+        });
+
+        map.add(featureLayer_2);
+  const featureLayer_1 = new FeatureLayer({
+          url: "http://services2.arcgis.com/S8zZg9pg23JUEexQ/arcgis/rest/services/RIDOT_Roads_2016/FeatureServer"
+        });
+
+        map.add(featureLayer_1);
+});
